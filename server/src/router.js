135fa30b05789ff8eb31ea766caf42577c67ede6
Riley Isom <rileyisom99@gmail.com>
2023-01-17 22:56:39 -0700

    Add basic authentication using jsonwebtoken. Fix bug with .env file being in wrong place. Test that http requests succeed and fail when they're supposed to.

--- server/src/router.js
+++ server/src/router.js
@@ -3,11 +3,39 @@ const express = require('express');
 
 const router = express.Router();
 
+// MIDDLEWARE
+isLoggedIn = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send('Invalid credentials');
+  } else {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) {
+        res.status(403).send('Invalid credentials');
+      } else {
+        next();
+      }
+    })
+  }
+  res.send('sup');
+};
+
+// ROUTER ENDPOINTS
 router.post('/login', (req, res) => {
-  res.send('hello');
+  if (req.body.password === process.env.PASSWORD) {
+    const token = jwt.sign({
+      userId: 1,
+    }, process.env.SECRET);
+    res.json({
+      token,
+    });
+  } else {
+    res.status(401).send('Incorrect password');
+  }
 });
 
-router.get('/tasks', (req, res) => {
+router.get('/tasks', isLoggedIn, (req, res) => {
   res.send('sup');
 });
 
