6916f87a6fa0f83adb9b484c97bf636b1fdadd0e
Riley Isom <rileyisom99@gmail.com>
2023-01-21 19:21:33 -0700

    Fix bug with filter rendering incorrectly. Rebuild and deploy project.

--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,34 +1,26 @@
 import React, { useContext, useState } from 'react';
-import { useQueryClient, useMutation, useQuery} from 'react-query';
+import { useQuery } from 'react-query';
 import ClipLoader from 'react-spinners/ClipLoader';
 
-import {TaskItem} from '../components/TaskItem';
+import { TaskItem } from '../components/TaskItem';
 import { CreateTaskForm } from '../components/CreateTaskForm';
 import readTasksRequest from '../api/readTasksRequest';
 import { TokenContext } from '../App';
 
 export const TaskPage = () => {
-  const queryClient = new useQueryClient();
   const [token] = useContext(TokenContext);
   const [filter, setFilter] = useState('all');
 
-  const {isLoading, data: tasks, } = useQuery(
-    'tasks', 
+  const {isLoading, data: tasks, refetch} = useQuery(
+    ['tasks', filter], 
     () => readTasksRequest(filter, token)
   );
-
-  const {mutate: updateFilter} = useMutation(
-    (newFilter) => setFilter(newFilter), {
-    onSettled: () => {
-      queryClient.invalidateQueries('tasks');
-    }
-  });
 
   return (
     <div>
       <h1>Task List</h1>
       <select value={filter} onChange={(e) => {
-        updateFilter(e.target.value);
+        setFilter(e.target.value);
       }}>
         <option value="all">All tasks</option>
         <option value="completed">Completed</option>
